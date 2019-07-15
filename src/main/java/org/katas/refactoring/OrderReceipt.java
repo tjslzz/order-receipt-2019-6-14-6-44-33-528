21c3e245c18e3a58951466f96b1bba1b2220b7c7
JarryLi <3244188372@qq.com>
2019-07-15 21:16:37 +0800

    refactor:Eliminate redundant code of OrderReceipt

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -16,40 +16,47 @@ public class OrderReceipt {
 
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
-
         output = printHeaders(output);
-
         output = printCustomerInfo(output,order);
+        output = printLineItems(output);
+        return output.toString();
+    }
 
-        // prints lineItems
+    private StringBuilder printLineItems(StringBuilder output){
         double totalSalesTx = 0d;
         double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription());
-            output.append('\t');
+            output = addSpaceT(output);
             output.append(lineItem.getPrice());
-            output.append('\t');
+            output = addSpaceT(output);
             output.append(lineItem.getQuantity());
-            output.append('\t');
+            output = addSpaceT(output);
             output.append(lineItem.totalAmount());
-            output.append('\n');
+            output = addSpaceN(output);
 
-            // calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * Sales_Tax_RATE;
-            totalSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            totalAmount += lineItem.totalAmount() + salesTax;
+            totalSalesTx += calculateSalesTax(lineItem);
+            totalAmount += lineItem.totalAmount() + calculateSalesTax(lineItem);
         }
 
-        // prints the state tax
-        output.append("Sales Tax").append('\t').append(totalSalesTx);
-
-        // print total amount
-        output.append("Total Amount").append('\t').append(totalAmount);
-        return output.toString();
+        output = printStateTax(output,totalSalesTx);
+        output = printTotalAmout(output,totalAmount);
+        return output;
     }
 
+    private double calculateSalesTax(LineItem lineItem){
+        return lineItem.totalAmount() * Sales_Tax_RATE;
+    }
+
+    private StringBuilder printStateTax(StringBuilder output,double totalSalesTx){
+        output.append("Sales Tax").append('\t').append(totalSalesTx);
+        return output;
+    }
+
+    private StringBuilder printTotalAmout(StringBuilder output,double totalAmount){
+        output.append("Total Amount").append('\t').append(totalAmount);
+        return output;
+    }
 
     private StringBuilder printCustomerInfo(StringBuilder output, Order order) {
         output.append(order.getCustomerName());
@@ -59,6 +66,16 @@ public class OrderReceipt {
 
     public StringBuilder printHeaders(StringBuilder output){
         output.append("======Printing Orders======\n");
+        return output;
+    }
+
+    private StringBuilder addSpaceT(StringBuilder output){
+        output.append('\t');
+        return output;
+    }
+
+    private StringBuilder addSpaceN(StringBuilder output){
+        output.append('\n');
         return output;
     }
 }
